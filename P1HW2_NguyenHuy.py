69731f608eafc8303434e8b77e614462aac97d3a
HuyN2004 <103298928+HuyN2004@users.noreply.github.com>
2022-04-09 00:49:17 -0400

    Add files via upload

---
+++ P1HW2_NguyenHuy.py
@@ -0,0 +1,12 @@
+#Pizza Order Project
+#4/8/2022
+#CTI-110 P1HW2 - Pizza Order
+#Huy Nguyen
+student_numb = int(input("How many students do you want to order pizza for?"))
+pizza_slice= student_numb * 3
+pizza_need= pizza_slice / 6
+print('----Pizza Order--------')
+print('Number of Students:', student_numb)
+print('Pizza Slices Needed:', pizza_slice)
+print('Pizzas Needed :', pizza_need)
+print('---------------------------')

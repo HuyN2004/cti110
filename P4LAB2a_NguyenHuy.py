7f49305407b068152aa22d9381a046fe9b7619b2
HuyN2004 <103298928+HuyN2004@users.noreply.github.com>
2022-05-09 01:02:39 -0400

    Add files via upload

---
+++ P4LAB2a_NguyenHuy.py
@@ -0,0 +1,17 @@
+import turtle
+
+turtle.penup()
+turtle.backward(120)
+turtle.pendown()
+
+for i in range(4):
+    turtle.forward(90)
+    turtle.left(90)
+
+turtle.penup()
+turtle.forward(180)
+turtle.pendown()
+
+for i in range(3):
+    turtle.forward(80)
+    turtle.left(120)

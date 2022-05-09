11f6abaf66a1a81c52a58b0337902cb680749bc5
HuyN2004 <103298928+HuyN2004@users.noreply.github.com>
2022-05-09 13:54:16 -0400

    Add files via upload

---
+++ P4HW1_NguyenHuy.py
@@ -0,0 +1,42 @@
+# CTI-110
+# P4HW1 - Score List
+# Huy Nguyen    
+# 5/9/2022
+#
+#Asking for scores count 
+count=int(input("How many scores do you want to enter? "))
+#Assigning i and mylist
+i,mylist=1,[]
+#Create loops
+while(True):
+    if(len(mylist)==count): 
+        break
+    print("Enter score #"+str(i)+": ",end="")
+    score = int(input())
+    if(score<0 or score>100):
+        print("\nINVALID Score entered!!!!")
+        print("Score should be between 0 and 100")
+    else:
+        mylist.append(float(score))
+        i+=1 
+#Sorting the list 
+mylist.sort()
+lowest_score = mylist[0]
+mylist.remove(lowest_score)
+score_avg=sum(mylist)/len(mylist)
+#Assign score_avg and grade
+if(score_avg>90):
+    grade = "A"
+elif(score_avg>=80 and score_avg<90):
+    grade = "B"
+elif(score_avg>=70 and score_avg<80):
+    grade = "C"
+else:
+    grade = "F"
+#Results 
+print("--------------Results-----------")
+print("Lowest Score      :",lowest_score)
+print("Modified List     :",mylist)
+print("Scores Average    :",score_avg)
+print("Grade             :",grade)
+print("-----------------------------------")

2df4e1241340def92a3eaf629cc313c009a16c91
HuyN2004 <103298928+HuyN2004@users.noreply.github.com>
2022-05-09 01:51:49 -0400

    Add files via upload

---
+++ P4LAB2b_NguyenHuy.py
@@ -0,0 +1,31 @@
+import turtle
+#Moving Back
+turtle.penup()
+turtle.backward(180)
+turtle.pendown()
+#Set up pensize and color
+turtle.pensize(5)
+turtle.color("lightpink")
+#Draw a H
+turtle.left(90)
+turtle.forward(90)
+turtle.backward(180)
+turtle.forward(90)
+turtle.right(90)
+turtle.forward(70)
+turtle.left(90)
+turtle.forward(90)
+turtle.backward(180)
+turtle.right(90)
+#Make a space
+turtle.penup()
+turtle.forward(70)
+turtle.pendown()
+#Draw a N
+turtle.left(90)
+turtle.forward(180)
+turtle.right(165)
+turtle.forward(185)
+turtle.left(165)
+turtle.forward(180)
+ 

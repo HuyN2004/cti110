05cfae8d2a415c6e64c263dd2d4d996d9b0792cd
HuyN2004 <103298928+HuyN2004@users.noreply.github.com>
2022-05-10 00:47:48 -0400

    Add files via upload

--- P4HW1_NguyenHuy.py
+++ P4HW1_NguyenHuy.py
@@ -6,24 +6,24 @@
 #Asking for scores count 
 count=int(input("How many scores do you want to enter? "))
 #Assigning i and mylist
-i,mylist=1,[]
+i, mylist=1, []
 #Create loops
 while(True):
     if(len(mylist)==count): 
         break
-    print("Enter score #"+str(i)+": ",end="")
+    print("Enter score #" + str(i) + ": ", end="")
     score = int(input())
     if(score<0 or score>100):
         print("\nINVALID Score entered!!!!")
         print("Score should be between 0 and 100")
     else:
         mylist.append(float(score))
-        i+=1 
+        i += 1 
 #Sorting the list 
 mylist.sort()
 lowest_score = mylist[0]
 mylist.remove(lowest_score)
-score_avg=sum(mylist)/len(mylist)
+score_avg=sum(mylist) / len(mylist)
 #Assign score_avg and grade
 if(score_avg>90):
     grade = "A"
